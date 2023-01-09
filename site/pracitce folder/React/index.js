709b55c28ac438874b8869b53cedf1609e001aa7
drazerrr <anuragdwivedi9794@gmail.com>
2023-01-09 13:15:29 +0530

    Use StUse State to Toggle an Elementate to Toggle an Element

--- site/pracitce folder/React/index.js
+++ site/pracitce folder/React/index.js
@@ -1,34 +1,65 @@
-/*Use PropTypes to Define the Props You Expect
-React provides useful type-checking features to verify that components receive props of the correct type. For example, your application makes an API call to retrieve data that you expect to be in an array, which is then passed to a component as a prop. You can set propTypes on your component to require the data to be of type array. This will throw a useful warning when the data is of any other type.
+/*Use State to Toggle an Element
+Sometimes you might need to know the previous state when updating the state. However, state updates may be asynchronous - this means React may batch multiple setState() calls into a single update. This means you can't rely on the previous value of this.state or this.props when calculating the next value. So, you should not use code like this:
 
-It's considered a best practice to set propTypes when you know the type of a prop ahead of time. You can define a propTypes property for a component in the same way you defined defaultProps. Doing this will check that props of a given key are present with a given type. Here's an example to require the type function for a prop called handleClick:
+this.setState({
+  counter: this.state.counter + this.props.increment
+});
+Instead, you should pass setState a function that allows you to access state and props. Using a function with setState guarantees you are working with the most current values of state and props. This means that the above should be rewritten as:
 
-MyComponent.propTypes = { handleClick: PropTypes.func.isRequired }
-In the example above, the PropTypes.func part checks that handleClick is a function. Adding isRequired tells React that handleClick is a required property for that component. You will see a warning if that prop isn't provided. Also notice that func represents function. Among the seven JavaScript primitive types, function and boolean (written as bool) are the only two that use unusual spelling. In addition to the primitive types, there are other types available. For example, you can check that a prop is a React element. Please refer to the documentation for all of the options.
+this.setState((state, props) => ({
+  counter: state.counter + props.increment
+}));
+You can also use a form without props if you need only the state:
 
-Note: As of React v15.5.0, PropTypes is imported independently from React, like this: import PropTypes from 'prop-types';
+this.setState(state => ({
+  counter: state.counter + 1
+}));
+Note that you have to wrap the object literal in parentheses, otherwise JavaScript thinks it's a block of code.
 
-Define propTypes for the Items component to require quantity as a prop and verify that it is of type number.*/
+MyComponent has a visibility property which is initialized to false. The render method returns one view if the value of visibility is true, and a different view if it is false.
 
-const Items = (props) => {
-    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
-  };
-  
-  // Change code below this line
-  Items.propTypes = {quantity: PropTypes.number.isRequired};
-  
-  // Change code above this line
-  
-  Items.defaultProps = {
-    quantity: 0
-  };
-  
-  class ShoppingCart extends React.Component {
+Currently, there is no way of updating the visibility property in the component's state. The value should toggle back and forth between true and false. There is a click handler on the button which triggers a class method called toggleVisibility(). Pass a function to setState to define this method so that the state of visibility toggles to the opposite value when the method is called. If visibility is false, the method sets it to true, and vice versa.
+
+Finally, click the button to see the conditional rendering of the component based on its state.
+
+Hint: Don't forget to bind the this keyword to the method in the constructor!*/
+class MyComponent extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        visibility: false
+      };
+      // Change code below this line
+      this.toggleVisibility = this.toggleVisibility.bind(this);
+  
+      // Change code above this line
     }
+    // Change code below this line
+    toggleVisibility() {
+      this.setState(state => {
+        if(state.visibility === true){
+          return {visibility: false};
+        } else {
+          return {visibility: true};
+        }
+      })
+    }
+  
+    // Change code above this line
     render() {
-      return <Items />
+      if (this.state.visibility) {
+        return (
+          <div>
+            <button onClick={this.toggleVisibility}>Click Me</button>
+            <h1>Now you see me!</h1>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <button onClick={this.toggleVisibility}>Click Me</button>
+          </div>
+        );
+      }
     }
-  };
-  ReactDOM.render(<Items />, document.getElementById("new"));
+  }
