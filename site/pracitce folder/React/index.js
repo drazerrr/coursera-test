04da0cf1906dd248c0e145d6d1cb09a813d68465
drazerrr <anuragdwivedi9794@gmail.com>
2023-01-10 15:30:18 +0530

    Optimize Re-Renders with shouldComponentUpdate

--- site/pracitce folder/React/index.js
+++ site/pracitce folder/React/index.js
@@ -1,45 +1,48 @@
-/*Add Event Listeners
-The componentDidMount() method is also the best place to attach any event listeners you need to add for specific functionality. React provides a synthetic event system which wraps the native event system present in browsers. This means that the synthetic event system behaves exactly the same regardless of the user's browser - even if the native events may behave differently between different browsers.
+/*Optimize Re-Renders with shouldComponentUpdate
+So far, if any component receives new state or new props, it re-renders itself and all its children. This is usually okay. But React provides a lifecycle method you can call when child components receive new state or props, and declare specifically if the components should update or not. The method is shouldComponentUpdate(), and it takes nextProps and nextState as parameters.
 
-You've already been using some of these synthetic event handlers such as onClick(). React's synthetic event system is great to use for most interactions you'll manage on DOM elements. However, if you want to attach an event handler to the document or window objects, you have to do this directly.
+This method is a useful way to optimize performance. For example, the default behavior is that your component re-renders when it receives new props, even if the props haven't changed. You can use shouldComponentUpdate() to prevent this by comparing the props. The method must return a boolean value that tells React whether or not to update the component. You can compare the current props (this.props) to the next props (nextProps) to determine if you need to update or not, and return true or false accordingly.
 
-Attach an event listener in the componentDidMount() method for keydown events and have these events trigger the callback handleKeyPress(). You can use document.addEventListener() which takes the event (in quotes) as the first argument and the callback as the second argument.
-
-Then, in componentWillUnmount(), remove this same event listener. You can pass the same arguments to document.removeEventListener(). It's good practice to use this lifecycle method to do any clean up on React components before they are unmounted and destroyed. Removing event listeners is an example of one such clean up action.
+The shouldComponentUpdate() method is added in a component called OnlyEvens. Currently, this method returns true so OnlyEvens re-renders every time it receives new props. Modify the method so OnlyEvens updates only if the value of its new props is even. Click the Add button and watch the order of events in your browser's console as the lifecycle hooks are triggered.
 */
-class MyComponent extends React.Component {
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    // Change code below this line
+    return true;
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: ''
+      value: 0
     };
-    this.handleEnter = this.handleEnter.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.addValue = this.addValue.bind(this);
   }
-  // Change code below this line
-  componentDidMount() {
-
-  }
-  componentWillUnmount() {
-
-  }
-  // Change code above this line
-  handleEnter() {
-    this.setState((state) => ({
-      message: state.message + 'You pressed the enter key! '
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
     }));
-  }
-  handleKeyPress(event) {
-    if (event.keyCode === 13) {
-      this.handleEnter();
-    }
   }
   render() {
     return (
       <div>
-        <h1>{this.state.message}</h1>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
       </div>
     );
   }
-};
+}
   ReactDOM.render(<MyComponent />, document.getElementById("new"))
